8ffd19e60a9bb31d073adb516550d33c79b013d1
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-09-26 14:21:04 +0600

    get data from localstorage and display in DOnation Page

---
+++ src/Pages/Donation/DonCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DonCard = ({donation}) => {
+
+    
+    const {id, Picture, Category, Title, card_bg, category_bg, text_button_bg , price, description} = donation || {};
+
+    return (
+        <div>
+            <div>
+            <div style={{backgroundColor : card_bg}} className="relative flex w-full max-w-[48rem] h-[200px] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ">
+     <div className='w-[200px]'>
+        <img className='h-full w-full' src={Picture} alt="" />
+     </div>
+      
+      <div className="p-6">
+       
+      <div className=" px-3 py-3 h-[150px]">
+       <h1 className=" font-semibold inline-block px-2 rounded" style={{color:text_button_bg , backgroundColor: category_bg }}>{Category}</h1>
+      
+       <h4 style={{color:text_button_bg }} className=" block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+         {Title}
+       </h4>
+       <div>
+        
+       <Link to={`/products/${id}`}>
+        <button style={{backgroundColor : text_button_bg}} className='text-white px-6  mt-[20px] btn'>View Details</button>
+        </Link>
+       </div>
+     </div>       
+     </div>
+     
+
+    </div>
+            </div>
+        </div>
+    );
+};
+
+export default DonCard;
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DonCard from './DonCard';
 
 const Donation = () => {
+
+    const [donations , setDonations] = useState([])
+    const [noData , setNoData] = useState("")
+
+
+    useEffect(()=>
+    {
+        const donationProducts =   JSON.parse(localStorage.getItem('products'));
+
+        if(donationProducts){
+            setDonations(donationProducts);
+        }else
+        {
+            setNoData('No Data Found')
+        }
+    },[])
+
+
     return (
         <div>
-            Donation
+            <div>
+                {
+                    noData ? <h2 className='text-3xl font-bold flex justify-center items-center h-[70vh]'>{noData}</h2> : 
+                    <div>
+                        <div className='grid md:grid-cols-2 grid-cols-1 gap-6 my-[30px]'>
+                            {
+                                donations.map(donation => <DonCard donation={donation} key={donation.id}></DonCard>)
+                            }
+                        </div>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
